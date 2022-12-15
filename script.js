c0a93f189f0492655bb0f7a665e1f9fec5d3f529
Karim Guettech <karimguettech@gmail.com>
2022-12-15 16:39:50 +0100

    formatting

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 const aggiungiClasseSelected = (event) => {
   const elementoCliccato = event.target;
-
   elementoCliccato.classList.toggle("selected");
 };
 const deleteTask = (event) => {
   const task = document.getElementById("list");
-
   const elemCLick = event.target;
-
   const class0 = Array.from(event.target.classList).join();
-  console.log(event.target);
   const figlio = document.getElementsByClassName(class0);
-  console.log(figlio);
   const figlio1 = Array.from(figlio);
   for (let i = 0; i < figlio1.length; i++) {
     task.removeChild(figlio1[i]);
-    console.log(figlio1[i]);
   }
 };
 let i = 0;
@@ -35,7 +29,6 @@ const AggiungoTask = () => {
     newDiv.appendChild(newButton);
     newButton.innerText = "delete";
     document.getElementById("list").appendChild(newDiv);
-
     document.getElementById("list-item-input").value = "";
     i++;
   }
