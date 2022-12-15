bb103565fe9926b278edf4b07bbb3b90c563f49d
Karim Guettech <karimguettech@gmail.com>
2022-12-15 15:45:38 +0100

    complete javascript

---
+++ script.js
@@ -0,0 +1,41 @@
+const aggiungiClasseSelected = (event) => {
+  const elementoCliccato = event.target;
+
+  elementoCliccato.classList.toggle("selected");
+};
+const deleteTask = (event) => {
+  const task = document.getElementById("list-items");
+
+  const elemCLick = event.target;
+
+  const class0 = Array.from(event.target.classList).join();
+  console.log(event.target);
+  const figlio = document.getElementsByClassName(class0);
+  console.log(figlio);
+  const figlio1 = Array.from(figlio);
+  for (let i = 0; i < figlio1.length; i++) {
+    task.removeChild(figlio1[i]);
+    console.log(figlio1[i]);
+  }
+};
+let i = 0;
+const AggiungoTask = () => {
+  const valoreInputField = document.getElementById("list-item-input").value;
+  if (valoreInputField === "") {
+    alert("Non lasciare vuoto il testo");
+  } else {
+    const newDiv = document.createElement("div");
+    const newButton = document.createElement("button");
+    newButton.classList.add(`${i}`);
+    newDiv.classList.add("list-items");
+    newDiv.classList.add(`${i}`);
+    newDiv.innerText = valoreInputField;
+    newButton.onclick = deleteTask;
+    newDiv.onclick = aggiungiClasseSelected;
+    newDiv.appendChild(newButton);
+    document.getElementById("list-items").appendChild(newDiv);
+
+    document.getElementById("list-item-input").value = "";
+    i++;
+  }
+};
